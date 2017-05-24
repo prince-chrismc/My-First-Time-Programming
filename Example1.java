cb99797a4326f62ec8ab544a4e5782ae93625829
Chris Mc <prince.chrismc@gmail.com>
2017-05-23 21:51:29 -0400

    I tried java LOLOLOL

---
+++ Example1.java
@@ -0,0 +1,13 @@
+package example1;
+
+public class Example1 
+{
+    public static void main(String args[]) 
+    {   
+        for(int a = 0; a < 20; a = a+1)
+        {
+            System.out.print (a);
+            System.out.print ("\n");      
+    }
+}
+}
